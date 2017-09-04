323b3fa2453aa033f0afc188bf32919a74da81bd
Joynann <joynan112@gmail.com>
2017-09-04 09:43:42 -0300

    Adicionado o index como página de login

--- index.php
+++ index.php
@@ -6,8 +6,23 @@
 <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
 <link rel="stylesheet" href="css/style.css" media="screen">
 </head>
+<?php session_start() ?>
 <body class="bg-success">
-
+<div class="container">
+<div class="row">
+<div class="col-xs-4 col-xs-offset-4 bg-primary margin-radius padding">
+<h1 class="text-center text-danger">Cadastrar</h1>
+<form method="post" class="form-group" action="login.php">
+<label>E-Mail:</label>
+<input type="email" name="email" class="form-control" placeholder="Digite seu E_Mail" required>
+<label>Senha:</label>
+<input type="password" name="pass" class="form-control" placeholder="Digite sua senha" required>
+<button class="btn btn-danger form-control margin" value="submit">Login</button>
+</form>
+<a href="cadastrar.php"><button class="btn btn-warning form-control">Não tem uma conta?</button></a>
+</div>
+</div>
+</div>
 <!-- JavaScript -->
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
