fcb30161638948455a2b9ac95545b0b01390df7a
Samuel Pacheco <samuelpacheco_qds@hotmail.com>
2017-10-18 02:52:44 -0200

    Trocando ifs pra try catch
    Ficou 30 linhas maior, com as mesmas funcionalidades, porem com código mais profissional e organizado.

--- login.php
+++ login.php
@@ -1,18 +1,47 @@
 <?php
-session_start();
-if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['pass']) && !empty($_POST['pass'])){
-    require "config.php";
-    $conf = new Conf();
-    $email = $_POST['email'];
-    $pass = md5($_POST['pass']);
-    $conf->select("users", array("email"=>$email,"pass"=>$pass));
-    $_SESSION = $conf->resultArray();
-if($conf->resultRowCount() > 0){
-    header('Location: DGP.php');
-    exit;
-}else{
-    echo "Seu Email ou sua senha esta errado";
-}}else{
-    header("Location: index.php");
-    exit;
+try
+{
+	//Verificar se recebeu algum POST
+	if($_POST)
+	{
+		session_start();
+
+		//Se o e-mail esta vazio
+		if(empty($_POST['email']))
+		{
+			throw new Exception('Digite seu e-mail', true);
+		}
+
+		//Se a senha está vazia
+		if(empty($_POST['senha']))
+		{
+			throw new Exception('Digite sua senha', true);
+		}
+
+		//Tentando buscar o suuario no banco
+		require "config.php";
+		$conf = new Conf();
+		$email = $_POST['email'];
+		$pass = md5($_POST['pass']);
+		$conf->select("users", array("email"=>$email,"pass"=>$pass));
+		$_SESSION = $conf->resultArray();
+
+		//Obtendo o reusltado
+		$result = $conf->resultRowCount();
+
+		//Verificando resultado
+		if(!$result)
+		{
+			throw new Exception('Usuário ou senha inválidos. Verifique', true);
+		}
+
+		//Se chegou até aqui, esta tudo correto. Podemos fazer o login.
+		header('Location: DGP.php');
+		exit;
+	}
+}
+catch(Exception $e)
+{
+	//Qualquer erro que ocorrer nas verificações acima, sera mostrado aqui.
+	echo 'Erro no login: '.$e->getMessage();
 }
